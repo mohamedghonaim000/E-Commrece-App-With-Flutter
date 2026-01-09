6f24cd004bc5fc87ef3207302d8cea571b47f9d1
mohamedghonaim000 <mohamedmokhtar64232@gmail.com>
2026-01-09 02:16:10 +0200

    Adding Sign Up Feature

--- lib/Features/Screens/Sign Up/SignUp.dart
+++ lib/Features/Screens/Sign Up/SignUp.dart
@@ -4,8 +4,8 @@ import 'package:ecommerceproject/Core/Resources/App_Field.dart';
 import 'package:ecommerceproject/Core/Resources/Custom_Button.dart';
 import 'package:ecommerceproject/Features/Widgets/Custom_Password_Field.dart';
 import 'package:flutter/material.dart';
-class Signin extends StatelessWidget {
-  const Signin({super.key});
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,33 +18,24 @@ class Signin extends StatelessWidget {
               SizedBox(height: 56,),
               AppCard(),
               SizedBox(height: 62,),
-              AppField(hintText: "User Name", labelText: "Enter Your Name"),
+              AppField(hintText: "user@gmail.com", labelText: "Email"),
               SizedBox(height: 25,),
               CustomPasswordField(hintText: "Password", labelText: "Password"),
               SizedBox(height: 3,),
-              Row(
-                children: [
-                  Checkbox(
-                    value: false,
-                    onChanged: null,
-                  ),
-                  Text("Remember me"),
-                  Spacer(),
-                  Text("Forgot Your Password?")
-                ],
-              ),
-              SizedBox(height: 22,),
-              CustomButton(btnText: "Login", onPressed: (){}),
-              SizedBox(height: 13,),
+              CustomPasswordField(hintText: "Password", labelText: "Confirm Password "),
+              SizedBox(height: 3,),
+              SizedBox(height: 50,),
+              CustomButton(btnText: "Sign Up", onPressed: (){}),
+              SizedBox(height: 99,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Don`t Have Account?" ,textAlign: TextAlign.center, style: TextStyle(color: AppColor.SecondrayTextColor ,fontSize: 12),),
-                  Text("Sign Up" , textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w500 ,fontSize: 12),)
+                  Text("Already Have Account?" ,textAlign: TextAlign.center, style: TextStyle(color: AppColor.SecondrayTextColor ,fontSize: 12),),
+                  Text("Sign In" , textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w500 ,fontSize: 12),)
                 ],
               )
-
-
+      
+      
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerceproject/Features/Screens/Home/Home.dart';
 import 'package:ecommerceproject/Features/Screens/Sign%20In/SignIn.dart';
+import 'package:ecommerceproject/Features/Screens/Sign%20Up/SignUp.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Signin(),
+      home: SignUp(),
     );
   }
 }
